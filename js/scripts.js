783cf0a5cf6b2f0afd47236bc326c582c3f09d83
Chase-Stokes <chasehstokes@gmail.com>
2021-11-05 09:59:10 -0700

    test to adjust the price +8 if the size value is medium ~completed

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,12 @@ function Pizza(toppings, size, price) {
   this.price = 0
 }
 
-
+Pizza.prototype.modifyPrice = function(){
+  if (this.size === "medium"){
+    this.price += 8
+  }else{
+    this.price += 10
+  }
+}
 
 let pizzaOne = new Pizza(["anchovies", "pineapple"], "medium", 0);
